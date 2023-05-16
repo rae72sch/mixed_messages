cbdfd4e1c84c6b6b68f6379780d61dead0d9a31e
RachelS <rae72sch@hotmail.com>
2023-05-16 12:01:12 +0100

    random number function added

--- messages.js
+++ messages.js
@@ -1,6 +1,16 @@
+// Set up three arrays of messages to be returned randomly
 const firstMessage = ['Drink plenty of water.', 'Get outdoor sunlight at the start of the day.', 
     'Spend time in nature.', 'Move your body every day.', 'Spend time with your favourite people.'];
 const secondMessage = ['Eat food that supports your health.', 'Get plenty of sleep.', 
     'Do things to help others.', 'Appreciate the beauty in the world.', 'Spend time being creative.'];
 const thirdMessage = ['Learn new skills.', 'Manage your stress.', 'Eat plenty of fruit & veg.', 
     'Remember to enjoy life.', 'Play and have fun!'];
+
+// Set up a function to return a random item for each of the three arrays, and assign it to a variable
+
+let randomNum = array => {
+    return Math.floor(Math.random() * array.length);
+};
+
+console.log(randomNum(firstMessage));
+

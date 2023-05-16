8c4c392339dd5b77a3d9b91f83dc54b3d3ff7614
RachelS <rae72sch@hotmail.com>
2023-05-16 09:58:44 +0100

    Stored Message Components

---
+++ messages.js
@@ -0,0 +1,6 @@
+const firstMessage = ['Drink plenty of water.', 'Get outdoor sunlight at the start of the day.', 
+    'Spend time in nature.', 'Move your body every day.', 'Spend time with your favourite people.'];
+const secondMessage = ['Eat food that supports your health.', 'Get plenty of sleep.', 
+    'Do things to help others.', 'Appreciate the beauty in the world.', 'Spend time being creative.'];
+const thirdMessage = ['Learn new skills.', 'Manage your stress.', 'Eat plenty of fruit & veg.', 
+    'Remember to enjoy life.', 'Play and have fun!'];
